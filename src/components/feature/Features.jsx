262dc97ddf17ec2bf9c37f1807c349e180523fea
Pratik More <developer@pratikmore.com>
2023-06-15 19:55:24 -0400

    First Project Completed
    I'll modify it later with new content

--- src/components/feature/Features.jsx
+++ src/components/feature/Features.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 import './features.css';
-const Features = () => {
-  return (
-    <div>
-        Features
-    </div>
-  )
-}
 
-export default Features
+const Feature = ({ title, text }) => (
+  <div className="gpt3__features-container__feature">
+    <div className="gpt3__features-container__feature-title">
+      <div />
+      <h1>{title}</h1>
+    </div>
+    <div className="gpt3__features-container_feature-text">
+      <p>{text}</p>
+    </div>
+  </div>
+);
+
+export default Feature;
